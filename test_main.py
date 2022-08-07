a78fdf2f17a9be3c3a6ce804e67db855ce2d8905
Henrik Eliasson <benke97.eliasson@gmail.com>
2022-08-07 18:01:49 +0200

    Add files via upload

--- test_main.py
+++ test_main.py
@@ -47,103 +47,107 @@ polygon_select2.disconnect()
 #print('\nSelected points:')
 #print(polygon_select2.xys[polygon_select2.ind])
 cerium = polygon_select2.xys[polygon_select2.ind]
-
+phases = [platinum,cerium]
+names = ['platinum','cerium']
+triangulations = []
+for i in range(number_of_phases):
 #----------------------Click on edges in order--------------------------------------
-fig, ax = plt.subplots()
-coll = ax.scatter(platinum[:,0], platinum[:,1], color=["blue"]*len(platinum), picker = 5, s=[50]*len(platinum))
-edge_loop_idx = []
-start_pos = 0
+    fig, ax = plt.subplots()
+    coll = ax.scatter(phases[i][:,0], phases[i][:,1], color=["blue"]*len(phases[i]), picker = 5, s=[50]*len(phases[i]))
+    edge_loop_idx = []
+    start_pos = 0
 
 
-def on_pick(event):
-    global start_pos
-    print(start_pos,event.ind)
-    if np.array_equal(event.ind,start_pos):
-        plt.close(fig)
-    
-    if not start_pos:
-        print("test")
-        start_pos = event.ind
-    print(platinum[event.ind], "clicked")
+    def on_pick(event):
+        global start_pos
+        print(start_pos,event.ind)
+        if np.array_equal(event.ind,start_pos):
+            plt.close(fig)
+        
+        if not start_pos:
+            print("test")
+            start_pos = event.ind
+        print(phases[i][event.ind], "clicked")
 
-    if np.array_equal(coll._facecolors[event.ind,:], np.array([[0,0,1,1]])):
-        edge_loop_idx.append(event.ind)
-        coll._facecolors[event.ind,:] = (1,0,0,1)
-        coll._edgecolors[event.ind,:] = (1,0,0,1)
-    
-    elif np.array_equal(coll._facecolors[event.ind,:], np.array([[1,0,0,1]])):
-        coll._facecolors[event.ind,:] = (0,0,1,1)
-        coll._edgecolors[event.ind,:] = (0,0,1,1)
-    fig.canvas.draw()
+        if np.array_equal(coll._facecolors[event.ind,:], np.array([[0,0,1,1]])):
+            edge_loop_idx.append(event.ind)
+            coll._facecolors[event.ind,:] = (1,0,0,1)
+            coll._edgecolors[event.ind,:] = (1,0,0,1)
+        
+        elif np.array_equal(coll._facecolors[event.ind,:], np.array([[1,0,0,1]])):
+            coll._facecolors[event.ind,:] = (0,0,1,1)
+            coll._edgecolors[event.ind,:] = (0,0,1,1)
+        fig.canvas.draw()
 
-fig.canvas.mpl_connect('pick_event', on_pick)
-plt.show()
-fig.canvas.mpl_disconnect(coll)
-plt.close(fig)
-#print(edge_loop_idx)
-#print(edge_loop_idx[1])
-#----------------------------Generate PSLG-----------------------------------
+    fig.canvas.mpl_connect('pick_event', on_pick)
+    plt.show()
+    fig.canvas.mpl_disconnect(coll)
+    plt.close(fig)
+    #print(edge_loop_idx)
+    #print(edge_loop_idx[1])
+    #----------------------------Generate PSLG-----------------------------------
 
-f = open("particle.poly", "w")
-#-----
-f.write("# particle.poly" + '\n')
-f.write("#" + '\n')
-f.write("# A platinum particle with X points in 2D, no attributes, one boundary marker." + '\n')
-f.write(str(len(platinum)) + " 2" + " 0" + " 1" + '\n')
-f.write("# Perimeter" + '\n')
+    f = open(str(names[i]) + ".poly", "w")
+    #-----
+    f.write("# " + str(names[i]) + ".poly" + '\n')
+    f.write("#" + '\n')
+    f.write("# A" + str(names[i]) + "particle with X points in 2D, no attributes, one boundary marker." + '\n')
+    f.write(str(len(phases[i])) + " 2" + " 0" + " 1" + '\n')
+    f.write("# Perimeter" + '\n')
 
-for i in platinum:
-    atom_idx = np.where(np.all(platinum==i,axis=1))
-    print(atom_idx[0])
-    atom_idx = atom_idx[0]
-    if atom_idx in edge_loop_idx:
-        f.write(str(int(atom_idx+1)) + " " + str(platinum[atom_idx][0,0]) + " " + str(platinum[atom_idx][0,1]) + " 2" + '\n')
-    else:
-        f.write(str(int(atom_idx+1)) + " " + str(platinum[atom_idx][0,0]) + " " + str(platinum[atom_idx][0,1]) + " 0" + '\n')
+    for j in phases[i]:
+        atom_idx = np.where(np.all(phases[i]==j,axis=1))
+        print(atom_idx[0])
+        atom_idx = atom_idx[0]
+        if atom_idx in edge_loop_idx:
+            f.write(str(int(atom_idx+1)) + " " + str(phases[i][atom_idx][0,0]) + " " + str(phases[i][atom_idx][0,1]) + " 2" + '\n')
+        else:
+            f.write(str(int(atom_idx+1)) + " " + str(phases[i][atom_idx][0,0]) + " " + str(phases[i][atom_idx][0,1]) + " 0" + '\n')
 
-#for i in edge_loop_idx:
-    #print(platinum[i])
-    #f.write(str(edge_loop_idx.index(i)+1) + " " + str(platinum[i][0,0]) + " " + str(platinum[i][0,1]) + " 2" + '\n')
-f.write('\n')
-#-----
-f.write("# X segments, each with boundary marker." + '\n')
-f.write(str(len(edge_loop_idx)) + " 1" + '\n')
-f.write("# Perimeter" + '\n')
+    #for i in edge_loop_idx:
+        #print(platinum[i])
+        #f.write(str(edge_loop_idx.index(i)+1) + " " + str(platinum[i][0,0]) + " " + str(platinum[i][0,1]) + " 2" + '\n')
+    f.write('\n')
+    #-----
+    f.write("# X segments, each with boundary marker." + '\n')
+    f.write(str(len(edge_loop_idx)) + " 1" + '\n')
+    f.write("# Perimeter" + '\n')
 
-for i in edge_loop_idx:
-    vertex_idx = edge_loop_idx.index(i)
-    if vertex_idx+1 == len(edge_loop_idx):
-        next_value = edge_loop_idx[0]
-    else:
-        print(vertex_idx,len(edge_loop_idx))
-        print(int(vertex_idx+1))
-        next_value = edge_loop_idx[int(vertex_idx+1)] 
-    f.write(str(int(vertex_idx+1)) + " " + str(int(i+1)) + " " + str(int(next_value+1)) + " 2" + '\n')
-
-
+    for j in edge_loop_idx:
+        vertex_idx = edge_loop_idx.index(j)
+        if vertex_idx+1 == len(edge_loop_idx):
+            next_value = edge_loop_idx[0]
+        else:
+            print(vertex_idx,len(edge_loop_idx))
+            print(int(vertex_idx+1))
+            next_value = edge_loop_idx[int(vertex_idx+1)] 
+        f.write(str(int(vertex_idx+1)) + " " + str(int(j+1)) + " " + str(int(next_value+1)) + " 2" + '\n')
 
 
 
-#for i in edge_loop_idx:
-#    if edge_loop_idx.index(i) + 1 < len(edge_loop_idx):
-#        f.write(str(edge_loop_idx.index(i)+1) + " " + str(edge_loop_idx.index(i)+1) + " " + str((edge_loop_idx.index(i)+2)%(len(edge_loop_idx)+1)) + " 2" + '\n')
-#    else:
-#        f.write(str(edge_loop_idx.index(i)+1) + " " + str(edge_loop_idx.index(i)+1) + " " + " 1" + " 2" + '\n')
-f.write('\n')
-#-----
-f.write("# No holes" + '\n')
-f.write("0")
 
-f.close()
+
+    #for i in edge_loop_idx:
+    #    if edge_loop_idx.index(i) + 1 < len(edge_loop_idx):
+    #        f.write(str(edge_loop_idx.index(i)+1) + " " + str(edge_loop_idx.index(i)+1) + " " + str((edge_loop_idx.index(i)+2)%(len(edge_loop_idx)+1)) + " 2" + '\n')
+    #    else:
+    #        f.write(str(edge_loop_idx.index(i)+1) + " " + str(edge_loop_idx.index(i)+1) + " " + " 1" + " 2" + '\n')
+    f.write('\n')
+    #-----
+    f.write("# No holes" + '\n')
+    f.write("0")
+
+    f.close()
 
 #open and read the file after the appending:
-f = open("particle.poly", "r")
+    #f = open(str(names[i])+".poly", "r")
 #print(f.read())
 
 #---------Show constrained delaunay---------------------
-dir_path = os.path.dirname(os.path.realpath(__file__))
-particle = tr.load(dir_path, "particle")
-t = tr.triangulate(particle, 'pne')
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    particle = tr.load(dir_path, str(names[i]))
+    t = tr.triangulate(particle, 'pne')
+    triangulations.append(t)
 #print(t['neighbors'].tolist())
 #print(t['vertices'].tolist())
 #print(t['triangles'].tolist())
@@ -151,8 +155,8 @@ t = tr.triangulate(particle, 'pne')
 #print(t['segments'].tolist())
 #print(t['regions'].tolist())
 #print(t['triangle_attributes'].tolist())
-tr.compare(plt, particle, t)
-plt.show()
+    tr.compare(plt, particle, t)
+    plt.show()
 
 class Material:
 
@@ -167,6 +171,7 @@ class Material:
         self.edge_classes = []
         self.center_neighborhood_vectors = []
         self.vertex_displacements = []
+        self.trig_strain = []
         self.central_vertex = 0
         self.primitive_vectors = np.zeros((2,2))
         self.neighbors = triangle_object['neighbors'].tolist()
@@ -184,6 +189,7 @@ class Material:
         self.calculate_vertex_displacement()
         self.set_ideal_vertices()
         self.calculate_ideal_triangles()
+        self.calc_trig_strain()
 
     def get_length(self,idx_1,idx_2,*args):
         if 'ideal' in args:
@@ -358,7 +364,67 @@ class Material:
 
         print('hello')
 
-platin = Material(t)
+    def calc_trig_strain(self):
+        i = 0
+        for trig in self.triangles:
+            a_idx = self.get_edge_index(trig[0],trig[1])
+            b_idx = self.get_edge_index(trig[1],trig[2])
+            c_idx = self.get_edge_index(trig[2],trig[0])
+            class1a,class2a = self.edge_classes[a_idx]
+            class1b,class2b = self.edge_classes[b_idx]
+            class1c,class2c = self.edge_classes[c_idx]
+            
+            if class1a == 99 or class2a == 99:
+                a_length = self.get_length(trig[0],trig[1])
+            else:
+                #a_length = (np.linalg.norm(np.array(self.center_neighborhood_vectors[class1a]))+np.absolute(np.array(self.center_neighborhood_vectors[class2a])))/2
+                a_length = (math.sqrt(pow(self.center_neighborhood_vectors[class1a][0],2)+pow(self.center_neighborhood_vectors[class1a][1],2))
+                            + math.sqrt(pow(self.center_neighborhood_vectors[class2a][0],2)+pow(self.center_neighborhood_vectors[class2a][1],2)))/2
+            if class1b == 99 or class2b == 99:
+                b_length = self.get_length(trig[1],trig[2])
+            else:
+                #b_length = (np.linalg.norm(np.array(self.center_neighborhood_vectors[class1b]))+np.absolute(np.array(self.center_neighborhood_vectors[class2b])))/2
+                b_length = (math.sqrt(pow(self.center_neighborhood_vectors[class1b][0],2)+pow(self.center_neighborhood_vectors[class1b][1],2))
+                            + math.sqrt(pow(self.center_neighborhood_vectors[class2b][0],2)+pow(self.center_neighborhood_vectors[class2b][1],2)))/2            
+            if class1b == 99 or class2b == 99:
+                c_length = self.get_length(trig[2],trig[0])
+            else:
+                #c_length = (np.linalg.norm(np.array(self.center_neighborhood_vectors[class1c]))+np.absolute(np.array(self.center_neighborhood_vectors[class2c])))/2
+                c_length = (math.sqrt(pow(self.center_neighborhood_vectors[class1c][0],2)+pow(self.center_neighborhood_vectors[class1c][1],2))
+                            + math.sqrt(pow(self.center_neighborhood_vectors[class2c][0],2)+pow(self.center_neighborhood_vectors[class2c][1],2)))/2
+
+
+            print(self.center_neighborhood_vectors[class1a])
+            print(np.array(self.center_neighborhood_vectors[class1a]))
+            print(np.absolute(np.array(self.center_neighborhood_vectors[class1a])))
+            print(a_length,b_length,c_length)
+            ideal_area = self.calc_trig_area(a_length,b_length,c_length)
+            strain_percent = (1-self.triangle_areas[i]/ideal_area)*100
+            self.trig_strain.append(strain_percent)
+            i +=1
+
+
+platin = Material(triangulations[0])
+ceri = Material(triangulations[1])
+
+print(platin.trig_strain)
+#fig, ax = plt.subplots()
+#im_data = im.T
+
+
+
+#plt.figure()
+#plt.gca().set_aspect('equal')
+#plt.tripcolor(np.array(platin.vertices)[:,0], np.array(platin.vertices)[:,1], np.array(platin.triangles), facecolors=np.array(platin.trig_strain),cmap='coolwarm',alpha=0.4, edgecolors='k')
+#plt.imshow(im_data,cmap='Greys')
+#plt.colorbar()
+#plt.title('tripcolor')
+
+#plt.show()
+
+
+
+
 Xboi= np.array(platin.vertices)
 Xbaoo = np.array(platin.ideal_vertices)
 plt.scatter(Xboi[:,0], Xboi[:,1], c ="b")
@@ -408,9 +474,15 @@ origin = np.repeat(np.array([[np.array(platin.vertices[platin.central_vertex])[0
 
 fig, ax = plt.subplots()
 im_data = im.T
-imaa = ax.imshow(im_data,origin = 'lower',cmap = 'plasma')
-tr.plot(ax,**t)
-ax.quiver(*origin, V[:,0],V[:,1], color=['r','b','g','k','y','c'],angles='xy', scale_units='xy', scale=1)
+imaa = ax.imshow(im_data,origin = 'lower',cmap = 'gray')
+tr.plot(ax,**triangulations[0])
+tr.plot(ax,**triangulations[1])
+ax.quiver(x_pos,y_pos,x_dir,y_dir,angles='xy', scale_units='xy', scale=1, color='m')
+#ax.quiver(*origin, V[:,0],V[:,1], color=['r','b','g','k','y','c'],angles='xy', scale_units='xy', scale=1)
+ax.tripcolor(np.array(platin.vertices)[:,0], np.array(platin.vertices)[:,1], np.array(platin.triangles),facecolors=np.array(platin.trig_strain), cmap='coolwarm',alpha=0.2, edgecolors='k')
+ax.tripcolor(np.array(ceri.vertices)[:,0], np.array(ceri.vertices)[:,1], np.array(ceri.triangles),facecolors=np.array(ceri.trig_strain), cmap='coolwarm',alpha=0.2, edgecolors='k')
+PCM=ax.get_children()[2]
+plt.colorbar(PCM,ax=ax)
 plt.show()
 
 #----------------------------------------------------------------------------
