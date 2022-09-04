6bf416ee0307def4141f96030709ece96f3e7010
Eliasson <henrik.eliasson@empa.ch>
2022-09-04 17:29:37 +0200

    nada

--- Plotter.py
+++ Plotter.py
@@ -268,3 +268,69 @@ class Plotter:
         fig, ax = plt.subplots()
         ax.scatter(X_r[:,0],X_r[:,1])
         plt.show()
+
+    def rel_size_plot(self,names,strain=False,edges=False,**kwargs):
+        #print(strain)
+        im_data = self.im.T
+        fig, [ax1,ax2] = plt.subplots(nrows=2,ncols=1)
+        ax1.imshow(im_data,origin = 'lower',cmap = 'gray')
+        ax2.imshow(im_data,origin = 'lower',cmap = 'gray')
+        patches = []
+        colors = []
+        for name in names:
+            element = self.get_material(name)
+            #print(element.edge_classes)
+            #print(element.number_of_connections)
+            if colors == []:
+                colors = np.array(element.voronoi_rel_size)[element.voronoi_bulk]
+            else:
+                colors = np.append(colors,np.array(element.voronoi_rel_size)[element.voronoi_bulk])
+
+            for cell_nr in range(len(element.voronoi_bulk)):
+                polygon = Polygon(np.array(element.voronoi_verts)[np.array(element.voronoi_cells)[element.voronoi_bulk][cell_nr]], True)
+                patches.append(polygon)
+            a = [np.vstack((np.array(element.voronoi_verts)[seg])) for seg in element.voronoi_segs]
+            line_segments = LineCollection(a, linewidths=2,
+                                colors='k', linestyle='solid')
+            ax1.add_collection(line_segments)
+        p = PatchCollection(patches,alpha = 0.5,cmap='coolwarm')
+        p.set_array(colors,)
+        p.set_clim([-np.amax(np.abs(colors)),np.amax(np.abs(colors))])
+        #p.set_clim([-15,15])
+        ax1.add_collection(p)
+        fig.colorbar(p,ax=ax1)
+
+        vertices = []
+        triangles = []
+        trig_strain = []
+        for name in names:
+            element = self.get_material(name)
+            if vertices == []:
+                vertices = np.array(element.vertices)
+                triangles = np.array(element.triangles)
+                trig_strain = np.array(element.trig_rel_size)
+            else:
+                vertices = np.concatenate((vertices,np.array(element.vertices)),axis=0)
+                triangles = np.concatenate((triangles,np.array(element.triangles)+np.amax(triangles+1)),axis=0)
+                trig_strain = np.append(trig_strain,np.array(element.trig_rel_size))
+        for name in names:
+            element = self.get_material(name)
+            a = [np.vstack((np.array(element.vertices)[seg])) for seg in element.edges]
+            line_segments = LineCollection(a, linewidths=2,
+                                colors='k', linestyle='solid')
+            ax2.add_collection(line_segments)
+        if strain:        
+            p = ax2.tripcolor(vertices[:,0],vertices[:,1],triangles,facecolors=trig_strain, cmap='coolwarm',alpha=0.5, edgecolors='k')
+            fig.colorbar(p,ax=ax2)
+        else:
+            p = ax2.tripcolor(vertices[:,0],vertices[:,1],triangles,facecolors=trig_strain, cmap='coolwarm',alpha=0, edgecolors='k')
+        p.set_clim([-np.amax(np.abs(trig_strain)),np.amax(np.abs(trig_strain))])
+        #lägg till segments och verts
+
+        #boi = ax.tripcolor(np.concatenate((np.array(platin.vertices),np.array(ceri.vertices)),axis=0)[:,0],np.concatenate((np.array(platin.vertices),np.array(ceri.vertices)),axis=0)[:,1],
+        #np.concatenate((np.array(platin.triangles),np.array(ceri.triangles)+np.amax(np.array(platin.triangles)+1)),axis=0),facecolors=np.append(np.array(platin.trig_strain),
+        #np.array(ceri.trig_strain)), cmap='coolwarm',alpha=0.5, edgecolors='k')
+        #np.concatenate((np.array(platin.triangles),np.array(ceri.triangles)+np.amax(np.array(platin.triangles)+1)),axis=0)
+        plt.show()
+        #plot delaunay, optional arguments: include strain, plotstuff
+        #print('thelawny')
--- tester.py
+++ tester.py
@@ -4,6 +4,7 @@ import triangle as tr
 import os 
 from material import Material
 from Plotter import Plotter
+import matplotlib.pyplot as plt
 
 
 "Enter image and the names of the phases"
@@ -29,6 +30,8 @@ for n in range(len(names)):
     particle = tr.load(dir_path, str(names[n]))
     t = tr.triangulate(particle, 'pne')
     materials.append(Material(t))
+    tr.compare(plt, particle, t)
+    plt.show()
 
 
 a = Plotter(materials,names,im)
@@ -39,4 +42,5 @@ a.plot_strain_tensor(names)
 a.plot_voronoi(names,strain=True)
 a.plot_displacement_map(names)
 a.plot_delaunay(names,strain=True)
+a.rel_size_plot(names,strain=True)
 print(im.shape)
